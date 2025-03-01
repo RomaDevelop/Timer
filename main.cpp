d555a97e11308043fbfffbbd5a4b4b7a7a8d84c3
Roma_home <26rus_mri@mail.ru>
2025-03-01 23:26:11 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 int main(int argc, char *argv[])
 {
+#error сделать чтобы + и - добавляло вышестоящие или уменьшало нижние (типа 59 минут + 1 равно +час)
 	QApplication a(argc, argv);
 	QStringList args;
 	for(int i=1; i<argc; i++) args.push_back(argv[i]);
